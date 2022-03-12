5e600c1fc1326b02dc3a84c90071c245f39b2ff5
bellerbrock <bje24@cornell.edu>
2022-03-12 13:50:19 -0500

    refine and rename

--- summarize_by_plot.R
+++ summarize_by_plot.R
@@ -2,11 +2,12 @@ require(tidyverse)
 require(magrittr)
 library(data.table)
 
-# Settings
-
-date = ""
+# VARIABLES TO SET
 trial_name = "21MC010024CUNA03"
-base_path = "/home/bryan/Desktop/GRADER"
+plants_harvested_per_plot = 20
+person = "Russell"
+base_path = "~/Desktop/GRADER"
+
 
 # Read in data
 data.files  <- list.files(
@@ -109,65 +110,42 @@ colnames(data.use) <- c(
   "time"
 )
 
-
-
-
-# convert hundreths of inch to cm and 10ths of ounces to grams
+# Add metric conversions
 data.use$diametercm <- as.numeric(data.use$estimateddiameter) * 0.0254 # cm per 1/100 inch
 data.use$lengthcm <- as.numeric(data.use$estimatedlength) * 0.0254 # cm per 1/100 inch
 data.use$weightg <- as.numeric(data.use$estimatedweight) * 2.8349523 # g per 1/10 ounce
 
-# calculate max cylindrical volume in cubic cenimeters
-data.use$max.volumecc <- pi * (as.numeric(data.use$diametercm)/2)^2 * as.numeric(data.use$lengthcm)  # cyclindrical volume = pi*r2*h
- 
-# calculate blockiness as ratio of weight in grams to maximum possible cylindrical volume (based on length and diameter)
+# Add blockiness scores, where blockiness is the ratio of actual volume (from estimated weight) to maximum possible cylindrical volume (from length and diameter)
 # 1 = perfectly blockly (all of possible cylindrical volume filled), 0 = infinitely not blocky (none of possible cylindrical volume filled)
-data.use$blockiness <- data.use$weightg / data.use$max.volumecc
+data.use$max.volumecc <- pi * (as.numeric(data.use$diametercm)/2)^2 * as.numeric(data.use$lengthcm)  # cyclindrical volume = pi*r2*h
+data.use$blockiness <- round(data.use$weightg / data.use$max.volumecc, digits = 2)
 
-# colnames(data.use); str(data.use) #to check for the correct data
+
+# Create an empty dataframe for plot summaries
 plot.names = unique(data.use$plotname)                   # list of all plots
 descript.results = data.frame(plotname = plot.names )    # create an empty dataframe
 
-# function for color averaging
-average_color <- function(indiv_colors) {
-  # print(indiv_colors)
-  reds<- substr(indiv_colors, 5, 6)
-  greens<- substr(indiv_colors, 7, 8)
-  blues<- substr(indiv_colors, 9, 10)
-  rsquared <- strtoi(reds, base=16)^2
-  gsquared <- strtoi(greens, base=16)^2
-  bsquared <- strtoi(blues, base=16)^2
-  return(
-    rgb(
-      round(sqrt(mean(rsquared))),
-      round(sqrt(mean(gsquared))),
-      round(sqrt(mean(bsquared))),
-      maxColorValue=255
-    )
-  )
-}
-
-predominant_color <- function(indiv_colors) {
-  # print(names(sort(-table(ind.plot$color1))))
-  # print(names(sort(-table(ind.plot$color1)))[1])
-  predominant_color = names(sort(-table(ind.plot$color1)))[1]
-  red<- substr(predominant_color, 5, 6)
-  green<- substr(predominant_color, 7, 8)
-  blue<- substr(predominant_color, 9, 10)
-  rsquared <- strtoi(red, base=16)^2
-  gsquared <- strtoi(green, base=16)^2
-  bsquared <- strtoi(blue, base=16)^2
-  return(
-    rgb(
-      round(sqrt(rsquared)),
-      round(sqrt(gsquared)),
-      round(sqrt(bsquared)),
-      maxColorValue=255
-    )
-  )
-}
+# Define a function for color averaging
+# average_color <- function(indiv_colors) {
+#   # print(indiv_colors)
+#   reds<- substr(indiv_colors, 5, 6)
+#   greens<- substr(indiv_colors, 7, 8)
+#   blues<- substr(indiv_colors, 9, 10)
+#   rsquared <- strtoi(reds, base=16)^2
+#   gsquared <- strtoi(greens, base=16)^2
+#   bsquared <- strtoi(blues, base=16)^2
+#   return(
+#     rgb(
+#       round(sqrt(mean(rsquared))),
+#       round(sqrt(mean(gsquared))),
+#       round(sqrt(mean(bsquared))),
+#       maxColorValue=255
+#     )
+#   )
+# }
 
 
+# Populate output dataframe with plot summaries
 for (i in 1:length(plot.names)) {
   # sort based on defects, size and weight i=1
   ind.plot.all = subset(data.use, plotname == plot.names [i])
@@ -202,18 +180,45 @@ for (i in 1:length(plot.names)) {
   ind.plot.jumbo.27more = subset(ind.plot.jumbo, estimatedweight > 270 )
   ind.plot.no1.jumbo = rbind(ind.plot.no1,ind.plot.jumbo)
   
+  ### Summarize ontology traits
+  
+  descript.results[i,c("Number of plants harvested per NET plot|CO_331:0000679")] = plants_harvested_per_plot
+  descript.results[i,c("Number of storage roots after harvest per NET plot|CO_331:0000233")] = nrow(ind.plot)
+  descript.results[i,c("Number of non-commercial storage roots per NET plot|CO_331:0000217")] = nrow(ind.plot.cull)
+  descript.results[i,c("Number of commercial storage roots per NET plot|CO_331:0000214")] = nrow(ind.plot) - nrow(ind.plot.cull)
+  descript.results[i,c("Length to diameter ratio computation|CO_331:0000779")] = round(mean(ind.plot$aspectratio)/100, digits = 2)
+  descript.results[i,c("Total storage root weight per NET plot in kg|CO_331:0000237")] = round(sum(ind.plot$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of cull storage roots measuring kg per plot|CO_331:0000612")] = round(sum(ind.plot.cull$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of canner storage roots measuring kg per plot|CO_331:0000610")] = round(sum(ind.plot.canner$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of total US no. 1 storage roots measuring kg per plot|CO_331:0000609")] = round(sum(ind.plot.no1$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of 32 count US no. 1 storage roots measuring kg per plot|CO_331:0000616")] = round(sum(ind.plot.no1.18.1_22$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of 40 count US no. 1 storage roots measuring kg per plot|CO_331:0000615")] = round(sum(ind.plot.no1.14.1_18$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of 55 count US no. 1 storage roots measuring kg per plot|CO_331:0000614")] = round(sum(ind.plot.no1.9.5_14$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of 90 count US no. 1 storage roots measuring kg per plot|CO_331:0000613")] = round(sum(ind.plot.no1.5_9.4$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of jumbo storage roots measuring kg per plot|CO_331:0000611")] = round(sum(ind.plot.jumbo$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of commercial storage roots per NET plot in kg|CO_331:0000220")] = round(sum(ind.plot$weightg/1000) - sum(ind.plot.cull$weightg/1000), digits = 2)
+  descript.results[i,c("Weight of non-commercial storage roots per NET plot in kg|CO_331:0000223")] = round(sum(ind.plot.cull$weightg/1000), digits = 2)
+  descript.results[i,c("Percentage of commercial storage root|CO_331:0000682")] = round(100 * ((sum(ind.plot$weightg/1000) - sum(ind.plot.cull$weightg/1000)) / sum(ind.plot$weightg/1000)), digits = 2)
+  descript.results[i,c("timestamp")] = strftime(strptime(max(ind.plot.all$time), "%m%d%Y %H:%M:%S"))
+
+
+  ### Summarize other traits
+  
   ## summarize color within plot
-  descript.results[i,c("average_color")] = average_color(ind.plot$color1)
-  descript.results[i,c("predominant_color")] = predominant_color(ind.plot$color1)
+  # descript.results[i,c("average_color")] = average_color(ind.plot$color1)
+  predominant_color = names(sort(-table(ind.plot$color1)))[1]
+  descript.results[i,c("predominant_color")] = paste("#", substr(predominant_color, 5, 10), sep="")
+  # descript.results[i,c("red_component")] = strtoi(substr(predominant_color, 5, 6), base=16)
+  # descript.results[i,c("green_component")] = strtoi(substr(predominant_color, 7, 8), base=16)
+  # descript.results[i,c("blue_component")] = strtoi(substr(predominant_color, 9, 10), base=16)
   
   ## root numbers of individual groups
-  descript.results[i,c("Number of storage roots after harvest per NET plot|CO_331:0000233")] = nrow(ind.plot)
+  # descript.results[i,c("number_all")] = nrow(ind.plot)
   descript.results[i,c("number_trash")] = nrow(ind.plot.trash)
   descript.results[i,c("number_can_cull_no1")] = nrow(ind.plot.mid)
   descript.results[i,c("number_canner")] = nrow(ind.plot.canner)
   descript.results[i,c("number_cull_no1")] = nrow(ind.plot.cull_no1)
-  descript.results[i,c("Number of non-commercial storage roots per NET plot|CO_331:0000217")] = nrow(ind.plot.cull)
-  descript.results[i,c("Number of commercial storage roots per NET plot|CO_331:0000214")] = nrow(ind.plot) - nrow(ind.plot.cull)
+  # descript.results[i,c("number_cull")] = nrow(ind.plot.cull)
   descript.results[i,c("number_no1")] = nrow(ind.plot.no1)
   descript.results[i,c("number_no1.18.1_22")] = nrow(ind.plot.no1.18.1_22)
   descript.results[i,c("number_no1.14.1_18")] = nrow(ind.plot.no1.14.1_18)
@@ -289,7 +294,7 @@ for (i in 1:length(plot.names)) {
   
   ## individual roots
   ## mean, SD, median of aspectratio
-  descript.results[i,c("Length to diameter ratio computation|CO_331:0000779")] = round(mean(ind.plot$aspectratio)/100, digits = 2)
+  # descript.results[i,c("mean_LD_all")] = round(mean(ind.plot$aspectratio)/100, digits = 2)
   descript.results[i,c("mean_LD_canner")] = round(mean(ind.plot.canner$aspectratio)/100, digits = 2)
   descript.results[i,c("mean_LD_cull")] = round(mean(ind.plot.cull$aspectratio)/100, digits = 2)
   descript.results[i,c("mean_LD_no1")] = round(mean(ind.plot.no1$aspectratio)/100, digits = 2)
@@ -329,20 +334,20 @@ for (i in 1:length(plot.names)) {
   
   ## individual plots
   ## total weight of individual classes
-  descript.results[i,c("Total storage root weight per NET plot in kg|CO_331:0000237")] = sum(ind.plot$estimatedweight/10)
-  descript.results[i,c("Weight of canner storage roots measuring kg per plot|CO_331:0000610")] = sum(ind.plot.canner$estimatedweight/10)
+  # descript.results[i,c("weight_all")] = sum(ind.plot$estimatedweight/10)
+  # descript.results[i,c("weight_canner")] = sum(ind.plot.canner$estimatedweight/10)
   descript.results[i,c("weight_cull_no1")] = sum(ind.plot.cull_no1$estimatedweight/10)
-  descript.results[i,c("Weight of cull storage roots measuring kg per plot|CO_331:0000612")] = sum(ind.plot.cull$estimatedweight/10)
-  descript.results[i,c("Weight of non-commercial storage roots per NET plot in kg|CO_331:0000223")] = sum(ind.plot.cull$estimatedweight/10)
-  descript.results[i,c("Weight of total US no. 1 storage roots measuring kg per plot|CO_331:0000609")] = sum(ind.plot.no1$estimatedweight/10)
-  descript.results[i,c("Weight of 32 count US no. 1 storage roots measuring kg per plot|CO_331:0000616")] = sum(ind.plot.no1.18.1_22$estimatedweight/10)
-  descript.results[i,c("Weight of 40 count US no. 1 storage roots measuring kg per plot|CO_331:0000615")] = sum(ind.plot.no1.14.1_18$estimatedweight/10)
-  descript.results[i,c("Weight of 55 count US no. 1 storage roots measuring kg per plot|CO_331:0000614")] = sum(ind.plot.no1.9.5_14$estimatedweight/10)
-  descript.results[i,c("Weight of 90 count US no. 1 storage roots measuring kg per plot|CO_331:0000613")] = sum(ind.plot.no1.5_9.4$estimatedweight/10)
-  descript.results[i,c("Weight of jumbo storage roots measuring kg per plot|CO_331:0000611")] = sum(ind.plot.jumbo$estimatedweight/10)
+  # descript.results[i,c("weight_cull")] = sum(ind.plot.cull$estimatedweight/10)
+  # descript.results[i,c("weight_no1")] = sum(ind.plot.no1$estimatedweight/10)
+  # descript.results[i,c("weight_no1.18.1_22")] = sum(ind.plot.no1.18.1_22$estimatedweight/10)
+  # descript.results[i,c("weight_no1.14.1_18")] = sum(ind.plot.no1.14.1_18$estimatedweight/10)
+  # descript.results[i,c("weight_no1.9.5_14")] = sum(ind.plot.no1.9.5_14$estimatedweight/10)
+  # descript.results[i,c("weight_no1.5_9.4")] = sum(ind.plot.no1.5_9.4$estimatedweight/10)
+  # descript.results[i,c("weight_jumbo")] = sum(ind.plot.jumbo$estimatedweight/10)
   descript.results[i,c("weight_jumbo_22_27")] = sum(ind.plot.jumbo.22_27$estimatedweight/10)
   descript.results[i,c("weight_jumbo_27more")] = sum(ind.plot.jumbo.27more$estimatedweight/10)
-  descript.results[i,c("Weight of commercial storage roots per NET plot in kg|CO_331:0000220")] = sum(ind.plot$estimatedweight/10) - sum(ind.plot.cull$estimatedweight/10)
+  # Marketwt = sum(ind.plot$estimatedweight/10) - sum(ind.plot.cull$estimatedweight/10)
+  # descript.results[i,c("weight_market")] = Marketwt
   descript.results[i,c("weight_no1.jumbo")] = sum(ind.plot.no1.jumbo$estimatedweight/10)
   
   ## weight percentage of individual classes to overall
@@ -384,33 +389,73 @@ for (i in 1:length(plot.names)) {
   descript.results[i,c("percent_jumbowt_jumbo_27more")] = round(sum(ind.plot.jumbo.27more$estimatedweight)/sum(ind.plot.jumbo$estimatedweight), digits = 2)
 }
 
-colnames(descript.results)[1] <- "observationunit_name"
-ordered <- descript.results[order(descript.results$number_no1,decreasing = TRUE),]
 
-barplot(
-  height = ordered$number_no1,
-  names = ordered$observationunit_name,
-  col= ordered$average_color,
-  title="no1 root count",
-  las=2,
-  cex.lab=0.75
+# Write spbase-ready output to table and database format plot summary files
+
+outfile <- paste(trial_name, "_database_plot_summaries.csv")
+header = as.matrix(t(c("plot_name", "trait", "value", "timestamp", "person", "location", "number")))
+write.table(
+  header,
+  outfile, sep = ",",
+  col.names = FALSE,
+  row.names = FALSE,
+  quote = TRUE
 )
+plots <- descript.results$plotname
+traits <- colnames(descript.results)
+times <- descript.results$timestamp
 
-barplot(
-  height = ordered$number_no1,
-  names = ordered$observationunit_name,
-  col= ordered$predominant_color,
-  title="no1 root count",
-  las=2,
-  cex.lab=0.75
-)
+for (i in 1:nrow(descript.results)) {
+  for (j in 2:18) {
+    write.table(
+      as.matrix(t(c(plots[i], traits[j], descript.results[i,j], times[i], person, "HCR", 1))),
+      outfile,
+      append = TRUE,
+      sep = ",",
+      col.names = FALSE,
+      row.names = FALSE,
+      quote = TRUE
+    )
+  }
+}
 
-dim(descript.results)
+spbase <- descript.results[,c(1:18)]
+colnames(spbase)[1] <- "observationunit_name"
+write.csv(spbase, paste(trial_name, "_table_plot_summaries.csv", sep=""), row.names = F)
 
-hist(descript.results$number_all)
-hist(descript.results$number_canner)
-hist(descript.results$number_no1)
-hist(descript.results$number_jumbo)
-hist(descript.results$number_cull)
-hist(descript.results$percent_Markewt_no1)
+# Write all output to TRIALNAME_complete_plot_summaries file
+
+write.csv(descript.results, paste(trial_name, "_complete_plot_summaries.csv", sep=""), row.names = F)
+
+
+# colnames(descript.results)[1] <- "observationunit_name"
+# 
+# ordered <- descript.results[order(descript.results$number_no1,decreasing = TRUE),]
+
+# barplot(
+#   height = ordered$number_no1,
+#   names = ordered$observationunit_name,
+#   col= ordered$average_color,
+#   title="no1 root count",
+#   las=2,
+#   cex.lab=0.75
+# )
+# 
+# barplot(
+#   height = ordered$number_no1,
+#   names = ordered$observationunit_name,
+#   col= ordered$predominant_color,
+#   title="no1 root count",
+#   las=2,
+#   cex.lab=0.75
+# )
+# 
+# dim(descript.results)
+# 
+# hist(descript.results$number_all)
+# hist(descript.results$number_canner)
+# hist(descript.results$number_no1)
+# hist(descript.results$number_jumbo)
+# hist(descript.results$number_cull)
+# hist(descript.results$percent_Markewt_no1)
 
